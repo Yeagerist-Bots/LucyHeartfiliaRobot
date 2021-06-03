a7cdc21f6d3156a6dfd1ad15da4e1caa7f7dba6c
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-03 20:51:54 +0530

    Update __main__.py

--- GabiBraunRobot/__main__.py
+++ GabiBraunRobot/__main__.py
@@ -103,9 +103,9 @@ AI Chatbot, Anime, Music, Notes, Filters, NSFW and more!
 🎛 Reach out for support:@Yeageristbots . (https://images5.fanpop.com/image/photos/29600000/Don-t-Say-Lazy-k-on-29601806-500-281.gif)
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
- You can support the project via [Paytm](#) or by contacting @callme_pro\
- Supporting isnt always financial! \
- Those who cannot provide monetary support are welcome to help us develop the bot at ."""
+You can support the project via [Paytm](#) or by contacting @callme_pro\
+Supporting isnt always financial! \
+Those who cannot provide monetary support are welcome to help us develop the bot at ."""
 
 IMPORTED = {}
 MIGRATEABLE = []
